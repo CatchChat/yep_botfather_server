815904a2cd2320766814fd1fe8f664208e570437
tumayun <tumayun.2010@gmail.com>
2016-05-24 01:14:34 +0800

    Fix typing message

--- lib/yep_bot/objects/message.rb
+++ lib/yep_bot/objects/message.rb
@@ -52,7 +52,7 @@ module YepBot
         Faye.to_json(
           message_type: 'instant_state',
           message: {
-            state: '0',
+            state: 0,
             recipient_type: recipient_type,
             recipient_id: recipient_id
           }
