20f91a3aa934c157df2e407f9c0f9ff9223de798
tumayun <tumayun.2010@gmail.com>
2016-05-23 14:34:00 +0800

    Set repo_url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'yep_botfather_server'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:CatchChat/yep_botfather_server.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
